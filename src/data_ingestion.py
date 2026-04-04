18ec87d8ab19e80280b8ab1ed23416b3358a02d1
Avanindra Bose <avanindrabose0218@gmail.com>
2026-04-05 00:22:09 +0530

    Added Second Experiment

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -6,8 +6,10 @@ df = pd.read_csv("https://raw.githubusercontent.com/araj2/customer-database/mast
 
 df = df.iloc[:,3:]
 
-df = df[df['Length of Membership'] > 3]
+df = df[df['Length of Membership'] > 1]
+
+df.drop(columns = ['Avg. Session Length'],inplace= True)
 
 root_path = Path(__file__).parent.parent
-print(root_path)
+# print(root_path)
 df.to_csv(root_path/"data"/"customers.csv",index=False)
